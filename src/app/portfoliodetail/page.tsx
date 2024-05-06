a8019e54945a6a54f50a30998f3bc9b770d5f454
石田翔平 <ishidashouhei@ishidashouheinoMacBook-Pro.local>
2024-05-06 10:08:45 +0900

    4 commit

--- src/app/portfoliodetail/page.tsx
+++ src/app/portfoliodetail/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Header from "../components/layouts/Header";
+import Image from 'next/image';
 import Link from "next/link";
 
 export default function PortfolioDetail() {
@@ -10,7 +11,7 @@ export default function PortfolioDetail() {
       <div className="px-5 py-10">
         <div className="max-w-4xl mx-auto">
           <div className="mt-8 mb-8">
-            <img src="/img/image1.png" alt="プロジェクトの詳細画像" className="w-full max-w-sm mx-auto rounded-lg shadow-lg" />
+            <Image src="/img/image1.png" alt="プロジェクトの詳細画像" width={500} height={300} className="mx-auto rounded-lg shadow-lg" />
           </div>
           <div className="text-lg">
             <h1 className="text-2xl font-bold mb-3">プロジェクト名</h1>
@@ -21,21 +22,21 @@ export default function PortfolioDetail() {
             <div className="grid grid-cols-3 gap-4">
               <Link href="/memberdetail/daiki">
                 <div className="cursor-pointer">
-                  <img src="/img/avatar_1.png" alt="Daiki Nakamori" className="w-24 h-24 rounded-full" />
+                  <Image src="/img/avatar_1.png" alt="Daiki Nakamori" width={96} height={96} className="rounded-full" />
                   <p className="mt-2">Daiki Nakamori</p>
                   <p className="text-sm text-gray-600">インタラクティブデザイナー, Tokyo</p>
                 </div>
               </Link>
               <Link href="/memberdetail/emiko">
                 <div className="cursor-pointer">
-                  <img src="/img/avatar_2.png" alt="Emiko Kudo" className="w-24 h-24 rounded-full" />
+                  <Image src="/img/avatar_2.png" alt="Emiko Kudo" width={96} height={96} className="rounded-full" />
                   <p className="mt-2">Emiko Kudo</p>
                   <p className="text-sm text-gray-600">開発者, Tokyo</p>
                 </div>
               </Link>
               <Link href="/memberdetail/erika">
                 <div className="cursor-pointer">
-                  <img src="/img/avatar_3.png" alt="Erika Oba" className="w-24 h-24 rounded-full" />
+                  <Image src="/img/avatar_3.png" alt="Erika Oba" width={96} height={96} className="rounded-full" />
                   <p className="mt-2">Erika Oba</p>
                   <p className="text-sm text-gray-600">プロジェクトマネージャー, Tokyo</p>
                 </div>
